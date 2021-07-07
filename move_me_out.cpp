1cd549b1c799df0401a7bdf2795873d2362ca299
Karev Pavel <karevpavel133@gmail.com>
2021-07-07 09:41:13 +0300

    Publish 1 homework

---
+++ move_me_out.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yacopsae on 07/07/2021.
+//
+
+#include <iostream>
+#include "move_me_out.h"
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
