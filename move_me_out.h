3738e3ee56cca2a8460bc3fddd7547138f6135a9
Karev Pavel <karevpavel133@gmail.com>
2021-07-09 19:52:40 +0300

    Add pragma once, change gitignore

--- move_me_out.h
+++ move_me_out.h
@@ -2,10 +2,6 @@
 // Created by yacopsae on 07/07/2021.
 //
 
-#ifndef INC_01_HELLO_WORLD_MOVE_ME_OUT_H
-#define INC_01_HELLO_WORLD_MOVE_ME_OUT_H
+#pragma once
 
 void todo_move_me_out();
-
-
-#endif //INC_01_HELLO_WORLD_MOVE_ME_OUT_H
