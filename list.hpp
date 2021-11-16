1649d3b2f06178c1246148d76ca06c954fe41b06
yacopsae <karevpavel133@gmail.com>
2021-11-16 22:37:17 +0300

    Ok. worked.

--- list.hpp
+++ list.hpp
@@ -17,11 +17,12 @@ class CustomList : public Container<T> {
   void erase() override;
   int size() override;
 
-  T operator[](int index) {
+  T *operator[](int index) {
 	if (index >= _size) {
+	  return nullptr;
 	}
 
-	return *(firstElementPtr() + index);
+	return firstElementPtr() + index;
   }
 
   friend std::ostream &operator<<(std::ostream &os, const CustomList<T> &list) {
@@ -45,6 +46,7 @@ class CustomList : public Container<T> {
   int _currCapacity;
 
   T *firstElementPtr() const;
+  T *resize();
 };
 
 template<typename T>
@@ -57,25 +59,29 @@ void CustomList<T>::push_back(T t) {
 	  _lastElement++;
 	  *_lastElement = t;
 	} else {
-	  std::cout << "Resize array from [" << _currCapacity << "] capacity to ";
-	  _currCapacity += _currCapacity * 2/3;
-	  std::cout << "[" << _currCapacity << "]" << std::endl;
-	  auto newAllocatedBlock = new T[_currCapacity];
-	  const auto newfirstElementPtr = newAllocatedBlock;
-	  auto nextElement = firstElementPtr();
-	  auto lastElIndex = _size - 1;
-
-	  for (int i = 0; i <= lastElIndex; i++, nextElement++, newAllocatedBlock++) {
-		*newAllocatedBlock = *nextElement;
-	  }
-	  auto first = firstElementPtr();
-	  delete[] first;
-	  _lastElement = newfirstElementPtr + _size;
+	  _lastElement = resize() + _size;
 	  *_lastElement = t;
 	}
   }
   _size++;
-  std::cout << "Pushed: " << t << std::endl;
+}
+
+template<typename T>
+T *CustomList<T>::resize() {
+  std::cout << "Resize array from [" << _currCapacity << "] capacity to ";
+  _currCapacity += _currCapacity * 2 / 3;
+  std::cout << "[" << _currCapacity << "]" << std::endl;
+  auto newAllocatedBlock = new T[_currCapacity];
+  const auto newfirstElementPtr = newAllocatedBlock;
+  auto nextElement = firstElementPtr();
+  auto lastElIndex = _size - 1;
+
+  for (int i = 0; i <= lastElIndex; i++, nextElement++, newAllocatedBlock++) {
+	*newAllocatedBlock = *nextElement;
+  }
+  auto first = firstElementPtr();
+  delete[] first;
+  return newfirstElementPtr;
 }
 
 template<typename T>
@@ -85,7 +91,7 @@ int CustomList<T>::size() {
 
 template<typename T>
 void CustomList<T>::remove(int index) {
-  if (index + 1 > _size) {
+  if (index >= _size) {
 	return;
   }
   auto *removable = firstElementPtr() + index;
@@ -100,7 +106,21 @@ void CustomList<T>::remove(int index) {
 
 template<typename T>
 void CustomList<T>::insert(int index, T t) {
+  if (index >= _size) {
+	std::cout << "Cannot do this, man" << std::endl;
+  }
 
+  if (_size + 1 >= _currCapacity) {
+	_lastElement = resize() + _size;
+  } else {
+    _lastElement++;
+  }
+
+  auto currentElement = _lastElement;
+  for (int i = _size - 1; i >= index; currentElement--, i--) {
+	*currentElement = *(currentElement - 1);
+  }
+  *(firstElementPtr() + index - 1) = t;
   _size++;
 }
 
@@ -108,7 +128,6 @@ template<typename T>
 void CustomList<T>::erase() {
   auto addr = _lastElement - _size + 1;
   delete[] addr;
-  std::cout << "erased! " << std::endl;
   _lastElement = nullptr;
   _size = 0;
 }
--- main.cpp
+++ main.cpp
@@ -9,34 +9,30 @@ int main() {
   for (int i = 0; i <= 9; i++) {
 	list.push_back(i);
   }
-
-  std::cout << "Inserted " << list.size() << " elements" << std::endl;
+  std::cout << "Inserted " << list.size() << " elements:" << std::endl;
   std::cout << list << std::endl;
-
   list.remove(7);
   list.remove(5);
   list.remove(3);
-
-  std::cout << "Deleted indexes 7, 5, 3" << std::endl;
-
+  std::cout << "Deleted indexes 7, 5, 3:" << std::endl;
   std::cout << list << std::endl;
-
-  list.push_back(1);
-  list.push_back(6);
-  std::cout << list << std::endl;
-
   list.erase();
+  std::cout << "erased:" << std::endl;
   std::cout << list << std::endl;
-
-
-  for (int i = 1; i <= 50; i++) {
+  for (int i = 1; i <= 74; i++) {
 	list.push_back(i);
   }
-  std::cout << "Inserted " << list.size() << " elements" << std::endl;
-  std::cout << list << std::endl;
-  list.push_back(1);
+  std::cout << "Inserted " << list.size() << " elements:" << std::endl;
   std::cout << list << std::endl;
 
-  std::cout << "At Index 10 " << list[10] << std::endl;
+  std::cout << "At Index 10: " << list[10] << std::endl;
+
+  list.insert(0, 99);
+  std::cout << list << std::endl;
+  list.erase();
+  std::cout << "erased:" << std::endl;
+  std::cout << list << std::endl;
+
+
   return 0;
 }
